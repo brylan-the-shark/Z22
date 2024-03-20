c8173d2c95361e9023ecbd71257b5be7661d6ba1
Brylan The Shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-03-20 13:59:09 +0000

    Place braces on new line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,29 +3,37 @@
 // import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 
-public class Main {
-  public static void main(String[] args) {
+public class Main
+{
+  public static void main(String[] args)
+  {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter the number of elements in the array: ");
     int n = scanner.nextInt();
     int[] numbers = new int[n];
     System.out.println("Enter the elements of the array:");
-    for (int i = 0; i < n; i++) {
+    
+    for (int i = 0; i < n; i++)
+    {
       numbers[i] = scanner.nextInt();
     }
-    int max = maxValue(numbers);
-    System.out.println("The biggest number is: " + max);
+    
+    int max = minValue(numbers);
+    System.out.println("The smallest number is: " + max);
     scanner.close();
   }
 
-  private static int maxValue(int[] nums) {
-    int max = nums[0];
-    for (int ktr = 0; ktr < nums.length; ktr++) {
-      if (nums[ktr] > max) {
-        max = nums[ktr];
+  private static int minValue(int[] nums)
+  {
+    int min = nums[0];
+    for (int ktr = 0; ktr < nums.length; ktr++)
+    {
+      if (nums[ktr] < min)
+      {
+        min = nums[ktr];
       }
     }
-    return max;
+    return min;
   }
 
   // @Test
