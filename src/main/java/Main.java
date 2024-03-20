83f420354084a08d931d01952eb73ad8ffad56d8
Brylan The Shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-03-20 13:31:10 +0000

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,35 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the number of elements in the array: ");
+    int n = scanner.nextInt();
+    int[] numbers = new int[n];
+    System.out.println("Enter the elements of the array:");
+    for (int i = 0; i < n; i++) {
+      numbers[i] = scanner.nextInt();
+    }
+    int max = maxValue(numbers);
+    System.out.println("The biggest number is: " + max);
+    scanner.close();
+  }
+
+  private static int maxValue(int[] nums) {
+    int max = nums[0];
+    for (int ktr = 0; ktr < nums.length; ktr++) {
+      if (nums[ktr] > max) {
+        max = nums[ktr];
+      }
+    }
+    return max;
   }
 
   // @Test
   // void addition() {
-  //     assertEquals(2, 1 + 1);
+  // assertEquals(2, 1 + 1);
   // }
 }
